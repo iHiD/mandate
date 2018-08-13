98ddb71f7489058232c53bcb3483d0b8bfb8bf71
Andrew Radev <andrey.radev@gmail.com>
2018-08-13 15:25:41 +0300

    Allow keyword args for initialize_with

--- test/initializer_test.rb
+++ test/initializer_test.rb
@@ -12,11 +12,16 @@ class InitializerTest < Minitest::Test
     initialize_with :foo, :bar
   end
 
+  class KeywordStorer
+    include Mandate
+    initialize_with :foo, bar: ["default"]
+  end
+
   def test_initializes_properly_without_args
     BoringStorer.new
   end
 
-  def test_initializes_properly
+  def test_initializes_properly_with_args
     foo = "fooooo"
     bar = "baaarrrr"
     storer = ArgumentativeStorer.new(foo, bar)
@@ -24,9 +29,30 @@ class InitializerTest < Minitest::Test
     assert_equal bar, storer.send(:bar)
   end
 
+  def test_initializes_properly_with_keyword_args
+    foo = "fooooo"
+    bar = "baaarrrr"
+    storer = KeywordStorer.new(foo, bar: bar)
+    assert_equal foo, storer.send(:foo)
+    assert_equal bar, storer.send(:bar)
+  end
+
+  def test_initializes_properly_with_a_missing_keyword_arg
+    foo = "fooooo"
+    storer = KeywordStorer.new(foo)
+    assert_equal foo, storer.send(:foo)
+    assert_equal ["default"], storer.send(:bar)
+  end
+
   def test_raises_with_wrong_amount_of_args
     assert_raises ArgumentError do
       ArgumentativeStorer.new(nil)
+    end
+  end
+
+  def test_raises_with_wrong_keyword_args
+    assert_raises ArgumentError do
+      KeywordStorer.new("foo", qux: "bar")
     end
   end
 
