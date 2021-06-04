a31ce62d2dc6ace66d6ec1ce91c482685f2cdf19
Jeremy Walker <jez.walker@gmail.com>
2021-06-04 22:53:26 +0100

    Add POC for ActiveSupport::Notifications

--- lib/mandate.rb
+++ lib/mandate.rb
@@ -10,4 +10,34 @@ module Mandate
     base.extend(CallInjector)
     base.extend(InitializerInjector)
   end
+
+  def self.use_notifications!
+    Config.instance.use_notifications!
+  end
+
+  def self.use_notifications?
+    !!Config.instance.use_notifications
+  end
+
+  class Config
+    include Singleton
+
+    attr_reader :use_notifications
+
+    def use_notifications!
+      begin
+        require 'active_support/notifications'
+      rescue StandardError => e
+        puts "ActiveSupport must be in the Gemfile to use notifications"
+        raise e
+      end
+
+      @use_notifications = true
+    end
+
+    # Used in the tests
+    def do_not_use_notifications!
+      @use_notifications = false
+    end
+  end
 end
